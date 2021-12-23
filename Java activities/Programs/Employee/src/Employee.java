61cc34eddef598fdadfc5174ef11e6c80f4f6bb6
Madhurocks575 <91519460+Madhurocks575@users.noreply.github.com>
2021-12-23 20:46:13 +0530

    Add files via upload

---
+++ Java activities/Programs/Employee/src/Employee.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author balaj
+ *
+ */
+static void in()
+//	{
+		class Employee
+		{
+			float salary=20000;
+		}
+		class Programmer extends Employee
+		{
+			
+		int bonus=2000;
+		
+		public static void main(String arg[])
+		{
+			Programmer a=new Programmer();
+			System.out.println("Salary is:"+a.salary);
+			System.out.println("Bonus is:"+a.bonus);
+			
+		}
