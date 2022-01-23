9787a46a860e87ae2c829d04eba5b4c9cc82ff63
Madhurocks575 <91519460+Madhurocks575@users.noreply.github.com>
2022-01-23 16:55:44 +0530

    Add files via upload

---
+++ Assignment and Screenshots/Operators/Conditional/Program/Condiop/src/condiop.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author balaj
+ *
+ */
+public class condiop {
+ public static void main(String[] args) {
+  String out;
+  int  a = 6, b = 12;
+  out = a==b ? "Yes":"No";
+  System.out.println("Ans: "+out);
+ }
+}
